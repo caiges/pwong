3f154851eb8a832aeeeb5a8c218cf39bd1baf4cc
caiges <caige@fastmail.fm>
2015-03-29 20:43:23 -0700

    Use software rendering over opengl

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 extern crate sdl2;
 
-use sdl2::video::{Window, WindowPos, OPENGL};
-use sdl2::render::{RenderDriverIndex, ACCELERATED, Renderer};
+use sdl2::video::{Window, WindowPos, RESIZABLE};
+use sdl2::render::{RenderDriverIndex, SOFTWARE, Renderer};
 use sdl2::pixels::Color;
 use sdl2::keycode::KeyCode;
 
 pub fn main() {
     let sdl_context = sdl2::init(sdl2::INIT_VIDEO).unwrap();
 
-    let window = match Window::new("PWONG", WindowPos::PosCentered, WindowPos::PosCentered, 800, 600, OPENGL) {
+    let window = match Window::new("PWONG", WindowPos::PosCentered, WindowPos::PosCentered, 800, 600, RESIZABLE) {
         Ok(window) => window,
         Err(err) => panic!("failed to create window: {}", err)
     };
 
-    let renderer = match Renderer::from_window(window, RenderDriverIndex::Auto, ACCELERATED) {
+    let renderer = match Renderer::from_window(window, RenderDriverIndex::Auto, SOFTWARE) {
         Ok(renderer) => renderer,
         Err(err) => panic!("failed to create renderer: {}", err)
     };
