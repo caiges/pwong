e79f43674583d48be033af9deaddaf47a28550ef
Greg Hill <greg.hill@rackspace.com>
2015-04-13 18:38:05 -0500

    make paddles accelerate when key is held down

--- src/entities/paddle.rs
+++ src/entities/paddle.rs
@@ -1,30 +1,57 @@
+static MAXIMUM_SPEED: f32 = 60f32;
+static DIRECTION_UP: i32 = -1;
+static DIRECTION_DOWN: i32 = 1;
+
 pub struct Paddle {
 	pub x: i32,
 	pub y: i32,
-	pub old_y: i32,
+	pub max_y: i32,
 	pub width: i32,
-	pub height: i32
+	pub height: i32,
+    pub trajectory: f32
 }
 
 impl Paddle {
-	pub fn new(x: i32, y: i32, old_y: i32, width: i32, height: i32) -> Paddle {
-		Paddle{x: x, y: y, old_y: old_y, width: width, height: height}
+	pub fn new(x: i32, y: i32, max_y: i32, width: i32, height: i32) -> Paddle {
+		Paddle{x: x, y: y, max_y: max_y, width: width, height: height, trajectory: 10f32}
 	}
 
 	fn position(&self) -> [i32; 2] {
 		[self.x, self.y]
 	}
 
-	pub fn up(&mut self, mult: i32) {
-		self.old_y = self.y;
-		if(self.y > 0) {
-			self.y -= 1 * mult;
-		}
-	}
+	pub fn up(&mut self) {
+        self.move_with_acceleration(DIRECTION_UP)
+    }
 
-	pub fn down(&mut self, mult: i32) {
-		self.old_y = self.y;
-		self.y += 1 * mult;
+    pub fn down(&mut self) {
+        self.move_with_acceleration(DIRECTION_DOWN)
+    }
+
+    pub fn stop(&mut self) {
+        if self.trajectory < 0f32 {
+            self.trajectory = -10f32
+        }
+        else {
+            self.trajectory = 10f32
+        }
+    }
+
+    pub fn move_with_acceleration(&mut self, direction: i32) {
+        if (self.trajectory < 0f32) != (direction < 0) {
+            self.trajectory = (direction * 10) as f32;
+        }
+        else if self.trajectory.abs() < MAXIMUM_SPEED {
+            self.trajectory *= 1.2
+        }
+
+        let mut new_y = self.y + self.trajectory as i32;
+        if new_y > self.max_y - self.height {
+            new_y = self.max_y - self.height;
+        }
+        if new_y > 0 {
+			self.y = new_y;
+        }
 	}
 }
 
--- src/main.rs
+++ src/main.rs
@@ -12,22 +12,16 @@ use sdl2::keycode::KeyCode;
 use pwong::entities::paddle::{Paddle};
 
 fn draw_paddle(drawer: &mut RenderDrawer, paddle: &mut Paddle) {
-    while paddle.old_y != paddle.y {
-        drawer.set_draw_color(Color::RGB(0, 0, 0));
-        drawer.clear();
-        drawer.set_draw_color(Color::RGB(255, 157, 0));
-        drawer.draw_rect(Rect::new(paddle.x, paddle.old_y, paddle.width, paddle.height));
-        drawer.present();
-        if paddle.y > paddle.old_y {
-            paddle.old_y += 1;
-        } else {
-            paddle.old_y -= 1;
-        }
-    }
+    drawer.set_draw_color(Color::RGB(0, 0, 0));
+    drawer.clear();
+    drawer.set_draw_color(Color::RGB(255, 157, 0));
+    drawer.draw_rect(Rect::new(paddle.x, paddle.y, paddle.width, paddle.height));
+    drawer.present();
 }
 
 fn draw(drawer: &mut RenderDrawer, paddle1: &mut Paddle, paddle2: &mut Paddle) {
     draw_paddle(drawer, paddle1);
+    draw_paddle(drawer, paddle2);
 }
 
 pub fn main() {
@@ -43,17 +37,13 @@ pub fn main() {
         Err(err) => panic!("failed to create renderer: {}", err)
     };
 
-    let mut p1 = Paddle::new(0, 40, 40, 40, 100);
-    let mut p2 = Paddle::new(760, 40, 40, 40, 100);
-    let movement_multiplier = 40;
+    let mut p1 = Paddle::new(0, 40, 800, 40, 100);
+    let mut p2 = Paddle::new(760, 40, 800, 40, 100);
 
     let mut running = true;
     let mut event_pump = sdl_context.event_pump();
 
     while running {
-        // Limit to 60 FPS
-        thread::sleep_ms(17);
-        
         for event in event_pump.poll_iter() {
             use sdl2::event::Event;
 
@@ -62,16 +52,28 @@ pub fn main() {
                     running = false
                 },
                 Event::KeyDown { keycode: KeyCode::A, .. } => {
-                    p1.up(movement_multiplier);
+                    p1.up();
+                },
+                Event::KeyUp { keycode: KeyCode::A, .. } => {
+                    p1.stop();
                 },
                 Event::KeyDown { keycode: KeyCode::Z, .. } => {
-                    p1.down(movement_multiplier);
+                    p1.down();
+                },
+                Event::KeyUp { keycode: KeyCode::Z, .. } => {
+                    p1.stop();
                 },
                 Event::KeyDown { keycode: KeyCode::Quote, .. } => {
-                    p2.up(movement_multiplier);
+                    p2.up();
+                },
+                Event::KeyUp { keycode: KeyCode::Quote, .. } => {
+                    p2.stop();
                 },
                 Event::KeyDown { keycode: KeyCode::Slash, .. } => {
-                    p2.down(movement_multiplier);
+                    p2.down();
+                },
+                Event::KeyUp { keycode: KeyCode::Slash, .. } => {
+                    p2.stop();
                 },
                 _ => {}
             }
@@ -79,8 +81,10 @@ pub fn main() {
         // Do game-y things
 
         // Clear and redraw
+        thread::sleep_ms(17);
+
         let mut drawer = renderer.drawer();
-        
+
         draw(&mut drawer, &mut p1, &mut p2);
         //draw(&mut drawer, &mut p2);
         //move_paddle(&mut drawer, &mut p1);
