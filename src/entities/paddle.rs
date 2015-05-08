c5b830c53171fd3a4ffd86ea813598b3c9b00ff0
caiges <caige@fastmail.fm>
2015-05-07 19:28:22 -0700

    Begin adding trait for bounding box

--- src/entities/paddle.rs
+++ src/entities/paddle.rs
@@ -5,13 +5,14 @@ pub struct Paddle {
 	pub y: i32,
 	pub old_y: i32,
 	pub width: i32,
-	pub height: i32,
-	pub bounding_box: BoundingBox
+	pub height: i32
 }
+
+impl BoundingBox for Paddle {}
 
 impl Paddle {
 	pub fn new(x: i32, y: i32, old_y: i32, width: i32, height: i32) -> Paddle {
-		Paddle{x: x, y: y, old_y: old_y, width: width, height: height, bounding_box: BoundingBox::new(x, y, width, height)}
+		Paddle{x: x, y: y, old_y: old_y, width: width, height: height}
 	}
 
 	fn position(&self) -> [i32; 2] {
@@ -31,7 +32,6 @@ impl Paddle {
 	}
 
 	pub fn update(&mut self) {
-        self.bounding_box.update_position(self.x, self.y);
     }
 }
 
