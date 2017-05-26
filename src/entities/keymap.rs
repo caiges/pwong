c8b509ab12eef6ce046cc5e259c830a04faf08a8
caiges <caigesn@gmail.com>
2017-05-25 20:40:01 -0700

    Track the insane amount of API changes to get this to build.

--- src/entities/keymap.rs
+++ src/entities/keymap.rs
@@ -2,40 +2,40 @@ extern crate num;
 extern crate sdl2;
 extern crate time;
 
-use self::sdl2::keycode::KeyCode;
+use self::sdl2::keyboard::Keycode;
 use self::num::ToPrimitive;
 
-static KEY_COUNT : usize = 235;
+static KEY_COUNT: usize = 235;
 
 pub struct KeyPressMap {
-    pub pressed: [u64; 235]
+    pub pressed: [u64; 235],
 }
 
 impl KeyPressMap {
     pub fn new() -> KeyPressMap {
-        KeyPressMap{pressed: [0u64; 235]}
+        KeyPressMap { pressed: [0u64; 235] }
     }
 
-    fn key_to_index(&mut self, key: KeyCode) -> usize {
-        return key.to_isize().unwrap() as usize
+    fn key_to_index(&mut self, key: Keycode) -> usize {
+        return key.to_isize().unwrap() as usize;
     }
 
-    pub fn press(&mut self, key: KeyCode) {
+    pub fn press(&mut self, key: Keycode) {
         let i = self.key_to_index(key);
         if i <= KEY_COUNT {
             self.pressed[i] = time::precise_time_ns();
         }
     }
 
-    pub fn release(&mut self, key: KeyCode) {
+    pub fn release(&mut self, key: Keycode) {
         let i = self.key_to_index(key);
         if i <= KEY_COUNT {
             self.pressed[i] = 0u64;
         }
     }
 
-    pub fn last_pressed(&mut self, keys: &[KeyCode]) -> KeyCode {
-        let mut last_key = KeyCode::Unknown;
+    pub fn last_pressed(&mut self, keys: &[Keycode]) -> Keycode {
+        let mut last_key = Keycode::Unknown;
         let mut last_time = 0u64;
         for key in keys {
             let i = self.key_to_index(*key);
@@ -47,36 +47,36 @@ impl KeyPressMap {
         return last_key;
     }
 
-    pub fn is_pressed(&mut self, key: KeyCode) -> bool {
+    pub fn is_pressed(&mut self, key: Keycode) -> bool {
         let i = self.key_to_index(key);
         if self.pressed[i] > 0u64 {
-            return true
+            return true;
         }
-        return false
+        return false;
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use super::sdl2::keycode::KeyCode;
+    use super::sdl2::keyboard::Keycode;
 
     #[test]
     fn test_key_mapping() {
         let mut map = KeyPressMap::new();
-        map.press(KeyCode::A);
-        assert!(map.is_pressed(KeyCode::A) == true);
-        assert!(map.is_pressed(KeyCode::B) == false);
-        assert!(map.last_pressed(&[KeyCode::A, KeyCode::B]) == KeyCode::A);
+        map.press(Keycode::A);
+        assert!(map.is_pressed(Keycode::A) == true);
+        assert!(map.is_pressed(Keycode::B) == false);
+        assert!(map.last_pressed(&[Keycode::A, Keycode::B]) == Keycode::A);
 
-        map.press(KeyCode::B);
-        assert!(map.is_pressed(KeyCode::A) == true);
-        assert!(map.is_pressed(KeyCode::B) == true);
-        assert!(map.last_pressed(&[KeyCode::A, KeyCode::B]) == KeyCode::B);
+        map.press(Keycode::B);
+        assert!(map.is_pressed(Keycode::A) == true);
+        assert!(map.is_pressed(Keycode::B) == true);
+        assert!(map.last_pressed(&[Keycode::A, KeyCode::B]) == Keycode::B);
 
-        map.release(KeyCode::A);
-        assert!(map.is_pressed(KeyCode::A) == false);
-        assert!(map.is_pressed(KeyCode::B) == true);
-        assert!(map.last_pressed(&[KeyCode::A, KeyCode::B]) == KeyCode::B);
+        map.release(Keycode::A);
+        assert!(map.is_pressed(Keycode::A) == false);
+        assert!(map.is_pressed(Keycode::B) == true);
+        assert!(map.last_pressed(&[Keycode::A, Keycode::B]) == Keycode::B);
     }
 }
